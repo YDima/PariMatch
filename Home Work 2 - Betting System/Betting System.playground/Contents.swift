145fdce54af30fe905a29798244c2c1a3b62cb73
YDima3 <yurchenkodmytroofficial@gmail.com>
2020-12-18 00:22:47 +0200

    Update

--- Home Work 2 - Betting System/Betting System.playground/Contents.swift
+++ Home Work 2 - Betting System/Betting System.playground/Contents.swift
@@ -1,12 +1,5 @@
 import UIKit
 
-//Implement a simple betting system with the following features:
-//     - When authorized, a regular user should be able to place a new bet
-//     - A bet is a simple string which contains description of an event.
-//     - An authorized user should be able to print a list of placed bets
-//     - An authorized admin user should be able to browse all registered regular users
-//     - An admin user should be able to ban a regular user
-
 protocol SystemUser {
      associatedtype Username: Hashable
      associatedtype Password: Hashable
@@ -20,13 +13,13 @@ protocol SystemUser {
      
 }
 
-enum State: String, Equatable {
+enum State: Hashable {
      case authorized
      case unauthorized
      case banned
 }
 
-enum Role: String {
+enum Role: Hashable {
      case admin
      case regular
 }
@@ -36,13 +29,23 @@ enum UserError: Error {
      case AccountIsNotInThisSystem
      case PasswordIsDifferent
      case UserIsBanned
+     case AnotherUserInTheSystem
+     case CantBanAdmin
+     case ThereIsNoBetsPlaced
 }
 
-class User: SystemUser, Hashable {
+class User: SystemUser {
      var username: String
      var password: String
      var role: Role
      var state: State
+     
+     init() {
+          self.username = ""
+          self.password = ""
+          self.role = .regular
+          self.state = .unauthorized
+     }
      
      init(username: String, password: String, role: Role, state: State) {
           self.username = username
@@ -56,53 +59,59 @@ struct Bet {
      var bet: String
 }
 
-class Storage {
+struct Storage {
      var users: [User] = []
-     var bets: [User: [Bet]] = [:]
+     var bets: [Bet] = []
+     var userBets: [User.Username: [Bet]] = [:]
 }
 
 class BettingSystem {
      var users = Storage().users
+     var userBets = Storage().userBets
      var bets = Storage().bets
+     var currentSystemUser = User()
      
-     func isNew(user: User) -> Bool {
-          guard users.first(where: { $0.username == user.username }) != nil else {
+     func isNew(username: User.Username) -> Bool {
+          guard users.first(where: { $0.username == username }) != nil else {
                return true
           }
           return false
      }
      
-     func checkPassword(user: User) -> Bool {
-          guard users.first(where: { $0.username == user.username && $0.password == user.password }) != nil else {
-                    return false
-               }
-               return true
+     func checkPassword(user: User, password: User.Password) -> Bool {
+          guard users.first(where: { $0.username == user.username && $0.password == password}) != nil else {
+               return false
+          }
+          return true
      }
      
      func editUserState(user: User, newState: State) -> User {
           let user = users.first(where: { $0.username == user.username })
           user!.state = newState
-          users.removeAll(where: { $0.username == user!.username })
-          users.append(user!)
+          
           return user!
      }
      
-     func authorize(user: User) throws -> User{
+     func authorize(user: User) throws {
           if user.state == .unauthorized {
-               editUserState(user: user, newState: .authorized)
-               return user
+               currentSystemUser = editUserState(user: user, newState: .authorized)
           } else if user.state == State.banned {
                throw UserError.UserIsBanned
           } else {
-               return user
+               currentSystemUser = user
           }
           
      }
      
-     func register(_ user: User) throws -> User{
-          guard isNew(user: user) else{
+     func register(_ username: User.Username, _ password: User.Password, _ role: User.Role) throws {
+          guard !otherUserInTheSystem() else {
+               throw UserError.AnotherUserInTheSystem
+          }
+          guard isNew(username: username) else{
                throw UserError.AccountAlreadyExists
           }
+          
+          let user = User(username: username, password: password, role: role, state: .unauthorized)
           
           users.append(user)
           
@@ -111,94 +120,150 @@ class BettingSystem {
           } catch UserError.UserIsBanned {
                print("This account was banned")
           }
-          return user
           
      }
 
-     func login(_ user: User) throws {
-          guard !isNew(user: user) else {
+     func login(_ username: User.Username, _ password: User.Password) throws {
+          guard !otherUserInTheSystem() else {
+               throw UserError.AnotherUserInTheSystem
+          }
+          guard !isNew(username: username) else {
                throw UserError.AccountIsNotInThisSystem
           }
-          guard checkPassword(user: user) else {
+          let user = users.first(where: {$0.username == username})
+          
+          guard checkPassword(user: user!, password: password) else {
                throw UserError.PasswordIsDifferent
           }
           do {
-               try authorize(user: user)
+               try authorize(user: user!)
           } catch UserError.UserIsBanned {
                print("This account was banned")
           }
-          
      }
 
-     func logout(_ user: User) throws {
-          guard !isNew(user: user) else {
-               throw UserError.AccountIsNotInThisSystem
+     func logout() {
+          currentSystemUser = User()
+          bets = []
+     }
+     
+     func otherUserInTheSystem() -> Bool{
+          guard currentSystemUser.state == .unauthorized else {
+               return true
           }
-          
-          if user.state == State.authorized {
-               editUserState(user: user, newState: .unauthorized)
-          } else if user.state == State.banned {
-               throw UserError.UserIsBanned
-          } else {
+          return false
+     }
+     
+     func placeBet(bet: Bet) {
+          guard currentSystemUser.state == .authorized else {
                return
           }
+          guard currentSystemUser.role == .regular else {
+               return
+          }
+          bets.append(bet)
+          userBets.updateValue(bets, forKey: currentSystemUser.username)
      }
      
-     func placeBet() {
-          
+     func printAllBets() throws {
+          guard currentSystemUser.state == .authorized else {
+               return
+          }
+          guard currentSystemUser.role == .regular else {
+               return
+          }
+          let allBets = userBets[currentSystemUser.username]
+          guard allBets != nil else {
+               throw UserError.ThereIsNoBetsPlaced
+          }
+          let list = allBets!.map{$0.bet}.joined(separator: ";\n")
+          print(list)
      }
      
-     func printAllBets() {
-          
-     }
-     
-     func banRegularUser() {
-          
+     func banRegularUser(username: User.Username) throws {
+          guard currentSystemUser.state == .authorized else {
+               return
+          }
+          guard currentSystemUser.role == .admin else {
+               return
+          }
+          let user = users.first(where: {$0.username == username})
+          guard user != nil else {
+               throw UserError.AccountIsNotInThisSystem
+          }
+          guard user!.role != Role.admin else {
+               throw UserError.CantBanAdmin
+          }
+          users.first(where: {$0.username == username})?.state = .banned
+          print("User was banned.")
      }
 
      func browseUsers() {
+          guard currentSystemUser.state == .authorized else {
+               return
+          }
+          guard currentSystemUser.role == .admin else {
+               return
+          }
           let regular = users.filter({$0.role == .regular})
-          let allUsers = regular.map{$0.username }.joined(separator: "\n")
+          let allUsers = regular.map{ $0.username }.joined(separator: "\n")
           print(allUsers)
      }
      
 }
 
-var newUser = User(username: "New", password: "New", role: .regular, state: .unauthorized)
-var sys = BettingSystem()
-
-newUser = try sys.register(newUser)
-sys.browseUsers()
+var s = BettingSystem()
 var users = Storage().users
 
-print(newUser.state)
+do {
+     try s.register("New", "New", .admin)
+} catch UserError.AccountAlreadyExists {
+     print("This username is already taken, please choose another one")
+} catch UserError.AnotherUserInTheSystem {
+     print("There is another user in the system")
+}
 
+s.logout()
 
+do {
+     try s.register("regular", "New", .regular)
+} catch UserError.AccountAlreadyExists {
+     print("This username is already taken, please choose another one")
+} catch UserError.AnotherUserInTheSystem {
+     print("There is another user in the system")
+}
+//
+//s.logout()
 //
 //do {
-//     try user.register(&newUser)
-//} catch UserError.AccountAlreadyExists{
-//     print("This username is already taken, please choose another one")
-//}
-//print(user.users)
-//print(admin.users)
-//newAdmin.browseUsers()
-//
-//do {
-//     try user.login(&newUser)
+//     try s.login("New", "New")
 //} catch UserError.AccountIsNotInThisSystem {
 //     print("This account is not in the system, create a new one")
 //} catch UserError.PasswordIsDifferent {
 //     print("Password is different")
 //} catch UserError.UserIsBanned {
 //     print("This account was banned")
+//} catch UserError.AnotherUserInTheSystem {
+//     print("There is another user in the system")
+//} catch {
+//     print("Something")
 //}
 //
 //do {
-//     try user.logout(&newUser)
+//     try s.banRegularUser(username: "regular")
 //} catch UserError.AccountIsNotInThisSystem {
 //     print("This account is not in the system, create a new one")
-//} catch UserError.UserIsBanned {
-//     print("This account was banned")
+//} catch UserError.CantBanAdmin {
+//     print("You can't ban other admins!")
 //}
+//
+
+var bet = Bet(bet: "TampaBay Lightnings vs. Pittsburg Penguins")
+s.placeBet(bet: bet)
+
+do {
+     try s.printAllBets()
+} catch UserError.ThereIsNoBetsPlaced {
+     print("You don't have placed bets.")
+}
 
