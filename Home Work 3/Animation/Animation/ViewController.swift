72bf737e06272027adc1c80e6ebd963d522a02d6
Dmytro Yurchenko <ydima3@Dima-Yurchenko.local>
2020-12-25 12:29:33 +0200

    Added .gitignore

---
+++ Home Work 3/Animation/Animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Dmytro Yurchenko on 24.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+     override func viewDidLoad() {
+          super.viewDidLoad()
+          // Do any additional setup after loading the view.
+     }
+
+
+}
+
