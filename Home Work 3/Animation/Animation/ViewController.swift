151fb6ee10e225eadc6a5daaa44c9787fb2b2a02
Dmytro Yurchenko <ydima3@Dima-Yurchenko.local>
2020-12-26 11:58:22 +0200

    Home Work 3: Calculator, Size classes, Animation

--- Home Work 3/Animation/Animation/ViewController.swift
+++ Home Work 3/Animation/Animation/ViewController.swift
@@ -8,12 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+     
+     @IBOutlet weak var resizingView: UIView!
+     @IBOutlet weak var circleView: UIView!
+     @IBOutlet weak var colorView: UIView!
+     @IBOutlet weak var circles: UIView!
+     @IBOutlet weak var circle1: UIView!
+     @IBOutlet weak var circle3: UIView!
+     private var circleColor: UIView!
+     
      override func viewDidLoad() {
           super.viewDidLoad()
-          // Do any additional setup after loading the view.
+          
      }
+     
+     override func viewDidAppear(_ animated: Bool) {
+          super.viewDidAppear(animated)
+          
+          let colors = [UIColor.yellow, UIColor.orange, UIColor.red, UIColor.blue, UIColor.green, UIColor.purple]
 
+          circleColor = UIView(frame: CGRect(x: 50, y: 50, width: 100, height: 100))
+          circleColor.frame.size = CGSize(width: 100, height: 100)
+          circleColor.frame.origin = CGPoint(x: 50, y: 50)
+          circleColor.backgroundColor = .white
+          circleColor.layer.cornerRadius = 50
+          colorView.addSubview(circleColor)
+          
+          circle1.layer.cornerRadius = 50
+          circle3.layer.cornerRadius = 50
+          
+
+          UIView.transition(with: self.resizingView, duration: 1.0, options: [.curveLinear], animations: {
+               self.resizingView.transform = CGAffineTransform(translationX: -33/2, y: -33/2)
+               self.resizingView.frame.size.height = 133
+               self.resizingView.frame.size.width = 133
+          }, completion: nil)
+          
+          UIView.transition(with: self.circleView, duration: 1.0, options: [.curveLinear], animations: {
+               self.circleView.layer.cornerRadius = 50
+          }, completion: nil)
+          
+          var c = 0
+          Timer.scheduledTimer(withTimeInterval: 0.3, repeats: true) {
+               timer in
+               self.colorView.backgroundColor = colors[c]
+               self.circleColor.backgroundColor = colors.reversed()[c]
+               c += 1
+               if c == colors.count {
+                    timer.invalidate()
+                    self.colorView.backgroundColor = .systemYellow
+                    self.circleColor.backgroundColor = .white
+               }
+          }
+          
+          UIView.animate(withDuration: 1.0, delay: 1.0, options: [.curveLinear, .autoreverse, .repeat], animations: {
+               self.circles.transform = CGAffineTransform(rotationAngle: .pi)
+          }, completion: nil)
+          
+     }
 
 }
 
--- Home Work 3/Size Classes/Size Classes/ViewController.swift
+++ Home Work 3/Size Classes/Size Classes/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+     
+     @IBOutlet weak var view1: UIStackView!
+     @IBOutlet weak var view3: UIStackView!
+     @IBOutlet weak var redView: UIView!
+     @IBOutlet weak var orangeView: UIView!
+     @IBOutlet weak var greenView: UIView!
+     @IBOutlet weak var purpleView: UIView!
+     
+     var isRegularHeight:Bool{
+          return view.traitCollection.verticalSizeClass == .regular
+     }
+     var isRegularWidth:Bool{
+          return view.traitCollection.horizontalSizeClass == .regular
+     }
+     
+     func landscapeLayout(){
+          view1.axis = .horizontal
+          view3.axis = .horizontal
+     }
+     
+     func portraitLayout(){
+          view1.axis = .vertical
+          view3.axis = .vertical
+     }
+     
+     override func viewWillLayoutSubviews() {
+          let iPad = UIDevice.current.userInterfaceIdiom == .pad
+          let orientation = UIDevice.current.orientation
+          if iPad && (orientation == .landscapeLeft)||(orientation == .landscapeRight) {
+               if isRegularHeight && isRegularWidth {
+                    landscapeLayout()
+               } else {
+                    portraitLayout()
+               }
+          } else {
+               portraitLayout()
+          }
+     }
+     
+     override func viewWillAppear(_ animated: Bool) {
+          super.viewWillAppear(animated)
+          UIDevice.current.beginGeneratingDeviceOrientationNotifications()
+          
+     }
+     
+     override func viewWillDisappear(_ animated: Bool) {
+          super.viewWillDisappear(animated)
+          UIDevice.current.endGeneratingDeviceOrientationNotifications()
+     }
+     
      override func viewDidLoad() {
           super.viewDidLoad()
-          // Do any additional setup after loading the view.
+          redView.layer.cornerRadius = 13
+          orangeView.layer.cornerRadius = 13
+          greenView.layer.cornerRadius = 13
+          purpleView.layer.cornerRadius = 13
      }
-
-
+     
 }
 
